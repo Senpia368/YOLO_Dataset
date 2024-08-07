1f36163533cdb076fde65c65634934a287ff0cc8
Hussam Abubakr <hussam.sam12345@gmail.com>
2024-08-07 17:49:17 -0400

    Minor Edit to getFrameRange() Input

--- extract_frames.py
+++ extract_frames.py
@@ -9,14 +9,13 @@ def getFrameRange():
         takes starting time and ending time as input
         returns tuple with starting frame and ending frame
     '''
-    start_minute = int(input('Enter starting minute: '))
-    start_second = int(input('Enter starting second: '))
+    start_time = int(input('Enter starting time [mmss]: '))
+    end_time = int(input('Enter ending time [mmss]: '))
 
-    end_minute = int(input('Enter ending minute: '))
-    end_second = int(input('Enter ending minute: '))
 
-    start_second += start_minute*60
-    end_second += end_minute*60
+    start_second = (start_time % 100) + (start_time // 100) * 60
+    end_second = (end_time % 100) + (end_time // 100) * 60
+
 
     start_frame = start_second*60
     end_frame = end_second*60
