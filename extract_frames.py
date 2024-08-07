226562c16e34dc7ef532ce8a0d1b3ecbc5ce65eb
Hussam Abubakr <hussam.sam12345@gmail.com>
2024-08-07 17:40:13 -0400

    Edit extract_frames.py

--- extract_frames.py
+++ extract_frames.py
@@ -1,4 +1,8 @@
+'''
+This program takes in the start and end times as input and extracts every set number of frames within the interval
+'''
 import cv2
+import os
 
 def getFrameRange():
     '''
@@ -21,6 +25,43 @@ def getFrameRange():
 
 video_path = "merged_output_video.mp4"
 
+destination_path = 'Images'
+os.makedirs(destination_path, exist_ok=True)
+
+start_frame, end_frame = getFrameRange()
+
+save_interval = 15
+
+cap = cv2.VideoCapture(video_path)
+
+if not cap.isOpened():
+    print('Error: Could not open video.')
+    exit()
+
+cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+
+current_frame = start_frame
+saved_frame_count = 0
+
+while current_frame <= end_frame:
+    ret, frame = cap.read()
+
+    # break loop if there are no more frames
+    if not ret:
+        break
+
+    # Save the frame at the specified interval
+    if (current_frame - start_frame) % save_interval == 0:
+        frame_filename = os.path.join(destination_path, f'frame_{current_frame}.jpg')
+        cv2.imwrite(frame_filename, frame)
+        saved_frame_count += 1
+
+    current_frame += 1
+
+# Release the video capture object
+cap.release()
+
+print(f"Saved {saved_frame_count} frames to {destination_path}")
 
 
 
