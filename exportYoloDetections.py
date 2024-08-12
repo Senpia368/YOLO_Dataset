65575a0faef47f933c524696f4e17bd037afc384
Hussam Abubakr <hussam.sam12345@gmail.com>
2024-08-12 15:03:48 -0400

    Add cleanupFolder function

--- exportYoloDetections.py
+++ exportYoloDetections.py
@@ -4,8 +4,16 @@ This program saves the annotations from the yolo model in a .txt file for a set 
 from ultralytics import YOLO
 import os
 
+def cleanupFolder(folder_path, fileType):
+    '''This function removes unwanted files with the specified type in the folder'''
+    for i in os.listdir(folder_path):
+        if i.split('.')[-1] == fileType:
+            file_path = os.path.join(folder_path,i)
+            os.remove(file_path)
 # source folder
-image_folder_path = 'Images'
+image_folder_path = 'task_task 1_dataset_2024_08_08_19_07_23_yolo 1.1/obj_train_data'
+
+cleanupFolder(image_folder_path, 'txt')
 
 model = model = YOLO("models/yolov10x.pt")
 
